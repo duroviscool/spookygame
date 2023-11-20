222b811d619524beda1fc1a50e0c8e3a5b0264b1
durovfan <130583279+duroviscool@users.noreply.github.com>
2023-11-20 18:00:25 +0300

    Update sql_bd.py

--- sql_bd.py
+++ sql_bd.py
@@ -10,8 +10,8 @@ class DataBaseSQL():
     def set(self, name, score):
         db_score = self.get(name=name) #ссылка на метод
         if score is not  None or score > db_score:
-            self.cur.execute(f"DELETE FROM stocks WHERE name='{name}") #удаляем из stocks где имя = переменной name
-            self.cur.execute(f"INSERT INTO stocks VALUES ('{name}, {score}')") #вставляем в stocks значения
+            self.cur.execute(f"DELETE FROM stocks WHERE name='{name}'") #удаляем из stocks где имя = переменной name
+            self.cur.execute(f"INSERT INTO stocks VALUES ('{name}', '{score}')") #вставляем в stocks значения
             self.con.commit() #подтверждаем действие
 
     def get(self, name=None, limit=5):
@@ -23,5 +23,5 @@ class DataBaseSQL():
         return scores
 
     def create_table(self):
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS stocks (name text, score int''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS stocks (name text, score int)''')
         self.con.commit()
